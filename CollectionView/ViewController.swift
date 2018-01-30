c45ad28f608c3f32a745cdf952151d1a73af79d0
toanhtran <toanh.t.tran@gmail.com>
2018-01-30 11:52:57 -0800

    first commit

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var collectionData = ["Happy ⚡ ", "Tent ⛺", "Shrine ⛩", "Lucky ☘", "1 🏆", "2 🐸", "3 🍩", "4 😸", "5 🤡", "6 👾", "7 👻", "8 👩‍🎤", "9 🎸", "10 🍖", "11 🐯", "12 🌋"]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +25,17 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return collectionData.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+		if let label = cell.viewWithTag(100) as? UILabel {
+			label.text = collectionData[indexPath.row]
+		}
+		return cell
+	}
+	
+}
